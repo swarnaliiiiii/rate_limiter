5cd9acef4b93fec312323fca1339867acd79ff0a
swarnaliiiiii <oli2k3@gmail.com>
2026-01-01 19:12:57 +0530

    Update Redis client configuration to use environment variables and enhance docker-compose with env_file

--- app/storage/redis_client.py
+++ app/storage/redis_client.py
@@ -1,8 +1,8 @@
+import os
 import redis
 
 redis_client = redis.Redis(
-    host="localhost",
-    port=6379,
-    db=0,
+    host=os.getenv("REDIS_HOST", "localhost"),
+    port=int(os.getenv("REDIS_PORT", 6379)),
     decode_responses=True
 )
